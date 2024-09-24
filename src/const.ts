3ef26b43d167aa8637154997f4d007603a017be7
!PSYDUCK <inaysanjaya@gmail.com>
2024-09-24 11:23:25 +0700

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -2,7 +2,7 @@ export const TRIBE: string = "4cc96181-1cd3-4494-ae49-7b7cb0e81eff";
 
 // Blum Endpoint
 export const BLUM_GAME_DOMAIN = "https://game-domain.blum.codes";
-export const BLUM_GATEWAY = "https://gateway.blum.codes";
+export const BLUM_GATEWAY = "https://user-domain.blum.codes";
 export const BLUM_USER_DOMAIN = "https://user-domain.blum.codes";
 export const BLUM_WALLET_DOMAIN = "https://wallet-domain.blum.codes";
 export const BLUM_EARN_DOMAIN = "https://earn-domain.blum.codes";
